2528f39a852ec038c58fec5463d4ea0ae7fa625c
Jeyaseelan R <jeyaseelan.rajendran@handbuiltapps.com>
2025-10-05 20:44:58 +0530

    Update Header

--- src/Layouts/Header/MenuItems/index.jsx
+++ src/Layouts/Header/MenuItems/index.jsx
@@ -1,84 +1,100 @@
+import $ from "jquery-slim";
 import { get } from 'lodash';
 import React, { useState } from 'react';
-
-import { Link } from '../../../components';
-
-import style from "./style.module.css";
-import getFaIcon from '../../../helpers/icons';
 import { useLocation } from 'react-router-dom';
 
-const MenuItems = ({ items = [], isMobileView = false }) => {
+import { Link } from '../../../components';
+import getFaIcon from '../../../helpers/icons';
+
+import style from "./style.module.css";
+
+const MenuItems = ({ items = [], isMobileView = false, id, Element = React.Fragment }) => {
     const [activeMenu, setActiveMenu] = useState(false);
     const { pathname } = useLocation();
 
     const onMouseHover = (idx) => {
-        if (!isMobileView) {
+        if (isMobileView === false) {
             setActiveMenu(idx);
         }
     }
 
     const onMouseClick = (idx) => {
-        if (isMobileView) {
+        if (isMobileView === true) {
             setActiveMenu(idx);
+            const wrapper = $(`#subMenuWrapper${idx}`);
+            if (wrapper.outerHeight() > 0) {
+                wrapper.css('height', "0px");
+            } else {
+                const contentHeight = $(`#subMenuList${idx}`).outerHeight() + "px";
+                wrapper.css('height', contentHeight);
+            }
         }
     }
 
+    const Tag = Element;
+
     return (
-        <nav className={style.menuItemsWrapper}>
-            {
-                items.length > 0 && (
-                    <ul className={style.menuList}>
-                        {
-                            items.map((item, idx) => {
-                                const subMenuLinks = get(item, "sub_menu_links", []);
+        <nav className={style.menuItemsWrapper} id={id}>
+            <div id={id + "Inner"}>
+                <Tag>
+                    {
+                        items.length > 0 && (
+                            <ul className={style.menuList}>
+                                {
+                                    items.map((item, idx) => {
+                                        const subMenuLinks = get(item, "sub_menu_links", []);
 
-                                return (
-                                    <li
-                                        role='button'
-                                        key={`menu_item_${idx}`}
-                                        className={`${style.menuItem}${idx === activeMenu ? ` ${style.active}` : ''}`}
-                                        onMouseEnter={() => onMouseHover(idx)}
-                                        onMouseLeave={() => onMouseHover(false)}
-                                        onClick={() => onMouseClick(idx)}
-                                    >
-                                        <Link href={item.url} className={`${style.menuLink}${(pathname === item.url) || (pathname.startsWith(`/${item.label.toLowerCase()}/`)) ? ` ${style.inView}` : ''}`}>
-                                            <span>
-                                                {item.label}
-                                            </span>
-                                            {
-                                                subMenuLinks.length > 0 && (
-                                                    <span className={style.icon}>
-                                                        <i className={getFaIcon("CaretDown")}></i>
+                                        return (
+                                            <li
+                                                role='button'
+                                                key={`menu_item_${idx}`}
+                                                className={`${style.menuItem}${idx === activeMenu ? ` ${style.active}` : ''}`}
+                                                onMouseEnter={() => onMouseHover(idx)}
+                                                onMouseLeave={() => onMouseHover(false)}
+                                                onClick={() => onMouseClick(idx)}
+                                            >
+                                                <Link href={item.url} className={`${style.menuLink}${(pathname === item.url) || (pathname.startsWith(`/${(item.label || "").toLowerCase()}/`)) ? ` ${style.inView}` : ''}`}>
+                                                    <span>
+                                                        {item.label}
                                                     </span>
-                                                )
-                                            }
-                                        </Link>
-
-                                        {
-                                            subMenuLinks.length > 0 && (
-                                                <ul className={style.subMenuList}>
                                                     {
-                                                        subMenuLinks.map((subMenu, idx) => {
-
-                                                            return (
-                                                                <li key={`menu_item_${idx}`} className={style.subMenuItem}>
-                                                                    <Link className={`${style.subMenuLink}${pathname === subMenu.url ? ` ${style.inView}` : ''}`} href={subMenu.url}>
-                                                                        {subMenu.label}
-                                                                    </Link>
-                                                                </li>
-                                                            )
-                                                        })
+                                                        subMenuLinks.length > 0 && (
+                                                            <span className={style.icon}>
+                                                                <i className={getFaIcon("CaretDown")}></i>
+                                                            </span>
+                                                        )
                                                     }
-                                                </ul>
-                                            )
-                                        }
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                )
-            }
+                                                </Link>
+
+                                                {
+                                                    subMenuLinks.length > 0 && (
+                                                        <div className={style.subMenuListWrapper} id={`subMenuWrapper${idx}`}>
+                                                            <ul className={style.subMenuList} id={`subMenuList${idx}`}>
+                                                                {
+                                                                    subMenuLinks.map((subMenu, idx) => {
+
+                                                                        return (
+                                                                            <li key={`menu_item_${idx}`} className={style.subMenuItem}>
+                                                                                <Link className={`${style.subMenuLink}${pathname === subMenu.url ? ` ${style.inView}` : ''}`} href={subMenu.url}>
+                                                                                    {subMenu.label}
+                                                                                </Link>
+                                                                            </li>
+                                                                        )
+                                                                    })
+                                                                }
+                                                            </ul>
+                                                        </div>
+                                                    )
+                                                }
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        )
+                    }
+                </Tag>
+            </div>
         </nav>
     )
 }
--- src/Layouts/Header/index.jsx
+++ src/Layouts/Header/index.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import $ from "jquery-slim";
+import { useLocation } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 
 import { LogoLight } from "../../helpers/Images";
 import { BackGround, Center, Link } from "../../components";
@@ -11,9 +13,34 @@ import style from "./style.module.css";
 const Header = ({ winWidth }) => {
 
   const [isOpen, setIsOpen] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const ele = $(`#mobileMenuWrapper`);
+    const eleHeight = $(`#mobileMenuWrapperInner`);
+    if (isOpen) {
+      ele.css('height', eleHeight.outerHeight() + 'px');
+    } else {
+      ele.css('height', '0px');
+    }
+  }, [isOpen])
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname])
+
+  useEffect(() => {
+    if (winWidth > 992) {
+      setIsOpen(false);
+    }
+  }, [winWidth])
+
+  const onToggleMenu = () => {
+    setIsOpen(prev => !prev)
+  }
 
   return (
-    <header>
+    <header className={style.header}>
       <Center>
         <div className={style.content}>
           <div className={style.lightLogo}>
@@ -33,7 +60,7 @@ const Header = ({ winWidth }) => {
               <p className={style.number}>800 123 4567</p>
             </div>
           </Link>
-          <Link className={style.menu} onClick={() => setIsOpen(prev => !prev)}>
+          <Link className={style.menu} onClick={onToggleMenu}>
             {
               isOpen && (
                 <i className={getFaIcon("Close")}></i>
@@ -47,6 +74,11 @@ const Header = ({ winWidth }) => {
           </Link>
         </div>
       </Center>
+      {
+        (winWidth <= 992) && (
+          <MenuItems items={HEADER_DATA.menu_links} isMobileView={true} Element={Center} id="mobileMenuWrapper" />
+        )
+      }
     </header>
   )
 }
